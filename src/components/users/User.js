0d3571428edca8bdcf6057a7b65322eca0fca986
Milan P <miki-p@hotmail.com>
2019-11-18 22:25:16 +0100

    first commit

---
+++ src/components/users/User.js
@@ -0,0 +1,92 @@
+import React, { Fragment, useEffect, useContext } from "react";
+import Spinner from "../layout/Spinner";
+import { Link } from "react-router-dom";
+import Repos from "../repos/Repos";
+import GithubContext from "../../context/github/githubContext";
+
+const User = props => {
+  const githubContext = useContext(GithubContext);
+
+  const { getUser, getUserRepos, loading, user, repos } = githubContext;
+
+  useEffect(() => {
+    getUser(props.match.params.login);
+    getUserRepos(props.match.params.login);
+    //eslint-disable-next-line
+  }, []);
+
+  const {
+    name,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    company,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable
+  } = user;
+
+  if (loading) return <Spinner />;
+
+  return (
+    <Fragment>
+      <Link to="/" className="btn btn-light">
+        Back to search
+      </Link>
+      Hireable:{" "}
+      {hireable ? (
+        <i className="fas fa-check text-success"></i>
+      ) : (
+        <i className="fas fa-times-circle text-danger"></i>
+      )}
+      <div className="card grid-2">
+        <div className="all-center">
+          <img
+            src={avatar_url}
+            alt="avatar"
+            className="round-img"
+            style={{ width: "150px" }}
+          />
+          <h1>{name}</h1>
+          <p>{location}</p>
+        </div>
+        <div>
+          {bio && (
+            <Fragment>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </Fragment>
+          )}
+          <a href={html_url} className="btn btn-dark my-1">
+            Visit Github profile
+          </a>
+          <ul>
+            <li>
+              {login && <strong>Username:</strong>} {login}
+            </li>
+            <li>
+              {login && <strong>Company:</strong>} {company}
+            </li>
+            <li>
+              {login && <strong>Website:</strong>} {blog}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="card-text-center">
+        <div className="badge badge-primary">Folowers: {followers}</div>
+        <div className="badge badge-success">Folowing: {following}</div>
+        <div className="badge badge-light">Public repos: {public_repos}</div>
+        <div className="badge badge-dark">Public Gists: {public_gists}</div>
+      </div>
+      <Repos repos={repos} />
+    </Fragment>
+  );
+};
+
+export default User;
